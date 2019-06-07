8a8f0b6a3abacf14507b86adefe5ea602f0a0706
Alaska <vacumsio@gmail.com>
2019-06-07 13:13:30 +0300

    сделал Update абстрактным

--- Asteroids/Objects/BaseObject.cs
+++ Asteroids/Objects/BaseObject.cs
@@ -17,19 +17,6 @@ namespace Asteroids
 
         public abstract void Draw();
 
-        public virtual void Update()
-        {
-            Pos.X = Pos.X - Dir.X*2;
-
-            if (Pos.X < 0)
-                Pos.X = Game.GetWidth() + Size.Width;
-
-            //Pos.X = Pos.X + Dir.X;
-            //Pos.Y = Pos.Y + Dir.Y;
-            //if (Pos.X < 0) Dir.X = -Dir.X;
-            //if (Pos.X > Game.Width) Dir.X = -Dir.X;
-            //if (Pos.Y < 0) Dir.Y = -Dir.Y;
-            //if (Pos.Y > Game.Height) Dir.Y = -Dir.Y;
-        }
+        public abstract void Update();
     }
 }
